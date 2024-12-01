7c3e2a5599cd1725fde69d948c4aef71d00e6ae6
OctopuSSX <vladik0701@gmail.com>
2024-12-01 22:41:25 +0300

    This causes cancer

--- aoc2024/src/days/day1.rs
+++ aoc2024/src/days/day1.rs
@@ -2,6 +2,18 @@ use std::iter::zip;
 
 use aoc::{DayResult, DaySolver};
 
+fn parse_digit(n: u8) -> u32 {
+  n as u32 - 0x30
+}
+
+fn parse_num(n: &[u8]) -> u32 {
+  parse_digit(n[0]) * 10000 +
+  parse_digit(n[1]) * 1000 +
+  parse_digit(n[2]) * 100 +
+  parse_digit(n[3]) * 10 +
+  parse_digit(n[4])
+}
+
 pub struct Day;
 
 impl DaySolver for Day {
@@ -10,10 +22,10 @@ impl DaySolver for Day {
     let mut b = [0; 1000];
     
     for (i, (_a, _b)) in input.lines()
-      .map(|line| unsafe {
+      .map(|line| {
         (
-          u32::from_str_radix(&line[0..5], 10).unwrap_unchecked(),
-          u32::from_str_radix(&line[8..13], 10).unwrap_unchecked(),
+          parse_num(&line[0..5].as_bytes()),
+          parse_num(&line[8..13].as_bytes()),
         )
       })
       .enumerate()
@@ -32,23 +44,23 @@ impl DaySolver for Day {
 
   fn two(&self, input: &str) -> DayResult {
     let mut a = [0; 1000];
-    let mut b = [0; 100000];
+    let mut b = [0; 90000];
 
     for (i, (_a, _b)) in input.lines()
-      .map(|line| unsafe {
+      .map(|line| {
         (
-          u32::from_str_radix(&line[0..5], 10).unwrap_unchecked(),
-          u32::from_str_radix(&line[8..13], 10).unwrap_unchecked(),
+          parse_num(&line[0..5].as_bytes()),
+          parse_num(&line[8..13].as_bytes()),
         )
       })
       .enumerate()
     {
       a[i] = _a;
-      b[_b as usize] += 1;
+      b[_b as usize - 10000] += 1;
     }
 
     let sum = a.iter()
-      .fold(0, |acc, a| acc + a * b[*a as usize]);
+      .fold(0, |acc, a| acc + a * b[*a as usize - 10000]);
 
     DayResult::Success(sum as i64)
   }
