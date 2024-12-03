efd731362f8d473096a7b43944fe17ad26f65374
OctopuSSX <vladik0701@gmail.com>
2024-12-03 16:17:34 +0300

    2024 - day 3

--- aoc2024/src/days/day3.rs
+++ aoc2024/src/days/day3.rs
@@ -1,13 +1,48 @@
 use aoc::{DayResult, DaySolver};
+use regex::Regex;
 
 pub struct Day;
 
 impl DaySolver for Day {
-  fn one(&self, _: &str) -> DayResult {
-    DayResult::Todo
+  fn one(&self, input: &str) -> DayResult {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut sum = 0;
+
+    for cap in re.captures_iter(input) {
+      let a = cap.get(1).unwrap().as_str().parse::<i64>().unwrap();
+      let b = cap.get(2).unwrap().as_str().parse::<i64>().unwrap();
+      sum += a * b;
+    }
+
+    DayResult::Success(sum)
   }
 
-  fn two(&self, _: &str) -> DayResult {
-    DayResult::Todo
+  fn two(&self, input: &str) -> DayResult {
+    let re = Regex::new(r"(?P<mul>mul\((\d+),(\d+)\))|(?P<do>do\(\))|(?P<dont>don't\(\))").unwrap();
+
+    let mut sum = 0;
+    let mut yes = true;
+
+    for cap in re.captures_iter(input) {
+      if let Some(_) = cap.name("do") {
+        yes = true;
+        continue;
+      } else if let Some(_) = cap.name("dont") {
+        yes = false;
+        continue;
+      } else if let Some(_) = cap.name("mul") {
+        if !yes {
+          continue;
+        }
+
+        let a = cap.get(2).unwrap().as_str().parse::<i64>().unwrap();
+        let b = cap.get(3).unwrap().as_str().parse::<i64>().unwrap();
+        sum += a * b;
+        continue;
+      }
+    }
+
+    DayResult::Success(sum)
   }
 }
