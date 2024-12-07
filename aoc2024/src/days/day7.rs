b002a63c46d0563c1cf034b2c1bd4949b51206fd
OctopuSSX <vladik0701@gmail.com>
2024-12-08 01:57:43 +0300

    Optimize 2024 day 7
    For some weird reason, a recursive solution is ~9x faster for part 1, but is ~5x slower for part 2

--- aoc2024/src/days/day7.rs
+++ aoc2024/src/days/day7.rs
@@ -1,6 +1,6 @@
 use aoc::{DayResult, DaySolver};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy)]
 enum Operator {
   Add,
   Mul,
@@ -8,16 +8,39 @@ enum Operator {
 }
 
 impl Operator {
-  fn apply(&self, a: u64, b: u64) -> u64 {
+  fn apply(&self, mut a: u64, b: u64) -> u64 {
     match self {
       Operator::Add => a + b,
       Operator::Mul => a * b,
       Operator::Concat => {
-        let shift = b.ilog10();
-        a * 10_u64.pow(shift + 1) + b
+        let mut shift = b;
+
+        while shift > 0 {
+          a *= 10;
+          shift /= 10;
+        }
+
+        a + b
       }
     }
   }
+}
+
+fn check(target: u64, current: u64, next: u64, operands: &[u64]) -> bool {
+  recurse(target, Operator::Add.apply(current, next), operands)
+    || recurse(target, Operator::Mul.apply(current, next), operands)
+}
+
+fn recurse(target: u64, current: u64, operands: &[u64]) -> bool {
+  let [next, operands @ ..] = operands else {
+    return current == target;
+  };
+
+  if current > target {
+    return false;
+  }
+  
+  check(target, current, *next, operands)
 }
 
 pub struct Day;
@@ -30,28 +53,8 @@ impl DaySolver for Day {
       let (total, elements) = line.split_once(": ").unwrap();
       let total = total.parse::<u64>().unwrap();
       let elements = elements.split(' ').map(|e| e.parse::<u64>().unwrap()).collect::<Vec<_>>();
-      let mut operations = 0_usize;
-      
-      let operators = [Operator::Add, Operator::Mul];
-      let max_combinations = operators.len().pow(elements.len() as u32) as usize;
-
-      while operations < max_combinations {
-        let mut result = elements[0];
-        let mut current_operations = operations;
-
-        for i in 1..elements.len() {
-          let element = elements[i];
-          let operator = operators[current_operations % operators.len()];
-          current_operations /= operators.len();
-          result = operator.apply(result, element);
-        }
-
-        if result == total {
-          total_calibration += total;
-          break
-        }
-
-        operations += 1;
+      if recurse(total, 0, &elements) {
+        total_calibration += total
       }
     }
 
